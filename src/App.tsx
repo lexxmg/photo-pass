298df6ac674643f77ed3ac7d1e8f0051d0f09fc8
mac-book <lexx.mg@gmail.com>
2025-10-29 19:38:38 +0300

    edd photo edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,61 @@
-import { useState } from 'react';
+import { useState, type ChangeEvent } from 'react';
 //import { Slider } from '@mui/material';
 import Cropper from 'react-easy-crop';
 import { type Point, type Area } from 'react-easy-crop';
-import testPhoto from './assets/test_image.jpg';
+//import testPhoto from './assets/test_image.jpg';
 
 export function App() {
   const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
   const [zoom, setZoom] = useState<number>(1);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
   const onCropComplete = (croppedArea: Area, croppedAreaPixels: Area) => {
     console.log(croppedArea, croppedAreaPixels);
+
+    if (croppedArea.height < 100 && croppedArea.width < 100) {
+      console.log('все верно');
+    } else {
+      console.log('ошибка, изображение длжно занимать все пространство внутри сетки');
+    }
+  };
+
+  const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file: File = e.target.files[0];
+      let imageDataUrl = await readFile(file);
+
+      if (imageDataUrl) {
+        setImageSrc(imageDataUrl as string);
+      }
+    }
   };
 
   return (
     <>
-      <h1 className="mt-5 text-center text-2xl">Фото к пропускам</h1>
-
-      <div className="relative m-auto mb-5 h-160 w-lg">
-        <Cropper
-          image={testPhoto}
-          crop={crop}
-          zoom={zoom}
-          // aspect={3 / 4}
-          cropSize={{ width: 300, height: 400 }}
-          objectFit={'cover'}
-          onCropChange={setCrop}
-          //onCropComplete={onCropComplete}
-          onZoomChange={setZoom}
-        />
+      <h1 className="mt-5 mb-5 text-center text-2xl">Фото к пропускам</h1>
+      <div className="relative m-auto mb-5 h-96 w-2xs bg-gray-400">
+        {imageSrc && (
+          <Cropper
+            image={imageSrc}
+            crop={crop}
+            zoom={zoom}
+            // aspect={3 / 4}
+            cropSize={{ width: 250, height: 300 }}
+            objectFit={'contain'}
+            onCropChange={setCrop}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+          />
+        )}
       </div>
+      <label className="m-auto block size-12 cursor-pointer bg-amber-300">
+        Выбрать фото
+        <input className="hidden" type="file" onChange={onFileChange} accept="image/*" />
+      </label>
 
       <div className="controls">
         <input
-          className="m-auto block w-lg"
+          className="slider m-auto block h-1 w-lg cursor-pointer appearance-none rounded-lg bg-gray-200"
           type="range"
           value={zoom}
           min={1}
@@ -44,4 +67,12 @@ export function App() {
       </div>
     </>
   );
+
+  function readFile(file: File) {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => resolve(reader.result), false);
+      reader.readAsDataURL(file);
+    });
+  }
 }
